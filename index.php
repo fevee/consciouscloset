15eb1dab447c259fbfa01516b2db0cbab04f577e
Faye Vaquilar <fe.tvaquilar@gmail.com>
2024-04-11 14:51:57 -0500

    nav bar update

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ $statement->execute();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
+    <script src="script.js"></script>
     <title>Conscious Closet</title>
 </head>
 <body>
--- nav.php
+++ nav.php
@@ -1,9 +1,17 @@
-<nav id="navbar">
-    <ul>
-        <li><a href="post.php">+ Post</a></li>
-        <li><a href="index.php">Shop Secondhand</a></li>
-        <li><a href="index.php">Certifications</a></li>
-        <li><a href="brands.php">Brands</a></li>
-        <li><a href="index.php">Home</a></li>
-    </ul>
-</nav>  
+<div class="navbar">
+    <a href="index.php">Home</a>
+    <a href="brands.php">Brands</a>
+    <a href="index.php">Certifications</a>
+    <a href="index.php">Shop Secondhand</a>
+    <div class="dropdown">
+        <button class="dropbtn">Admin
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="post.php">+Post</a>
+            <a href="add_brand.php">+Brand</a>
+            <a href="add_cert.php">+Certification</a>
+            <a href="add_item.php">+Shop Item</a>
+        </div>
+    </div>
+</div>
